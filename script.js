c23e6e7df7147b865cfc5ebda9c905851d96143c
markuskana23-lgtm <markuskana23@gmail.com>
2026-02-16 21:52:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// tombol scroll otomatis
+document.getElementById("scrollBtn").addEventListener("click", function() {
+
+document.getElementById("pengurus").scrollIntoView({
+behavior: "smooth"
+});
+
+});
